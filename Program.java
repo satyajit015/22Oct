b249a691360fbef64c777fbabbb9c68d18a28be6
satyajit <satyajit015@gmail.com>
2020-11-21 08:58:37 +0530

    changing-3 java

--- Program.java
+++ Program.java
@@ -1,5 +1,5 @@
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello Java - change1 -  8:47");	
+		System.out.println("Hello Java - change3 -  8:58");	
 	}
 }
